01b4a3c1bfc80551413202a09c792ec004d49965
Muhammad Rizky Millennianno <muhammad.rizky912@ui.ac.id>
2022-05-03 17:16:17 +0700

    Update pins

--- testing/ThermocoupleSensor/ThermocoupleSensor.ino
+++ testing/ThermocoupleSensor/ThermocoupleSensor.ino
@@ -1,8 +1,8 @@
 #include "max6675.h"
 
-#define MAX6675_CS 10
-#define MAX6675_MISO 12
-#define MAX6675_SCLK 13
+#define MAX6675_CS 15
+#define MAX6675_MISO 0
+#define MAX6675_SCLK 5
 
 MAX6675 thermocouple(MAX6675_SCLK, MAX6675_CS, MAX6675_MISO);
 
